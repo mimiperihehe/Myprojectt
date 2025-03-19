c533247efed9690cdc7c2ed6aedb4cd37de6230d
alvina <alvinaramadhan07@gmail.com>
2025-03-19 08:09:41 -0700

    add disini

---
+++ src/myprojectt/Myprojectt.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myprojectt;
+/**
+ *
+ * @author alvina
+ */
+public class Myprojectt {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
